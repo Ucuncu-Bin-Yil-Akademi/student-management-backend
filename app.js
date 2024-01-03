53d5f3ac1f283cefba61fc517eb137d4d622b84a
Canberk <highpriv@gmail.com>
2024-01-04 00:27:47 +0300

    documentation: swagger

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// app.js
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
@@ -17,19 +18,30 @@ mongoose.connect(
 );
 
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const studentRoutes = require("./routes/studentRoutes");
-const attendanceRoutes = require("./routes/attendanceRoutes");
-const classRoutes = require("./routes/classRoutes");
-
-app.use("/students", studentRoutes);
-app.use("/attendance", attendanceRoutes);
-app.use("/classes", classRoutes);
+app.use("/students", require("./routes/studentRoutes"));
+app.use("/attendance", require("./routes/attendanceRoutes"));
+app.use("/classes", require("./routes/classRoutes"));
 app.use("/instructors", require("./routes/instructorRoutes"));
 app.use("/admins", require("./routes/adminRoutes"));
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: "Student Management API",
+      version: "1.0.0",
+      description:
+        "API for managing students, attendance, classes, instructors, and admins",
+    },
+  },
+  apis: ["./routes/*.js"], // Path to your route files
+};
+
+const swaggerDocs = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,4 +1,124 @@
-// routes/studentRoutes.js
+/**
+ * @swagger
+ * tags:
+ *   - name: 'student'
+ *     description: 'Operations related to students'
+ *
+ * paths:
+ *   /student/create:
+ *     post:
+ *       tags:
+ *         - 'student'
+ *       summary: 'Create a new student'
+ *       consumes:
+ *         - 'application/json'
+ *       produces:
+ *         - 'application/json'
+ *       parameters:
+ *         - in: 'body'
+ *           name: 'studentData'
+ *           description: 'Student data to be created'
+ *           required: true
+ *           schema:
+ *             $ref: '#/definitions/Student'
+ *       responses:
+ *         '201':
+ *           description: 'Student created successfully'
+ *           schema:
+ *             $ref: '#/definitions/Student'
+ *         '400':
+ *           description: 'Bad request - Check required fields'
+ *         '500':
+ *           description: 'Internal server error'
+ *
+ *   /student/{id}/update:
+ *     put:
+ *       tags:
+ *         - 'student'
+ *       summary: 'Update a student by ID'
+ *       consumes:
+ *         - 'application/json'
+ *       produces:
+ *         - 'application/json'
+ *       parameters:
+ *         - in: 'path'
+ *           name: 'id'
+ *           type: 'string'
+ *           required: true
+ *           description: 'ID of the student to be updated'
+ *         - in: 'body'
+ *           name: 'studentData'
+ *           description: 'Updated student data'
+ *           required: true
+ *           schema:
+ *             $ref: '#/definitions/Student'
+ *       responses:
+ *         '200':
+ *           description: 'Student updated successfully'
+ *           schema:
+ *             $ref: '#/definitions/Student'
+ *         '400':
+ *           description: 'Bad request - Check required fields'
+ *         '500':
+ *           description: 'Internal server error'
+ *
+ *   /student/{id}/delete:
+ *     delete:
+ *       tags:
+ *         - 'student'
+ *       summary: 'Delete a student by ID'
+ *       produces:
+ *         - 'application/json'
+ *       parameters:
+ *         - in: 'path'
+ *           name: 'id'
+ *           type: 'string'
+ *           required: true
+ *           description: 'ID of the student to be deleted'
+ *       responses:
+ *         '204':
+ *           description: 'Student deleted successfully'
+ *         '500':
+ *           description: 'Internal server error'
+ *
+ *   /student:
+ *     get:
+ *       tags:
+ *         - 'student'
+ *       summary: 'Get all students'
+ *       produces:
+ *         - 'application/json'
+ *       responses:
+ *         '200':
+ *           description: 'List of students'
+ *           schema:
+ *             type: 'array'
+ *             items:
+ *               $ref: '#/definitions/Student'
+ *         '500':
+ *           description: 'Internal server error'
+ *
+ * definitions:
+ *   Student:
+ *     type: 'object'
+ *     properties:
+ *       _id:
+ *         type: 'string'
+ *       name:
+ *         type: 'string'
+ *       lastName:
+ *         type: 'string'
+ *       mail:
+ *         type: 'string'
+ *       phoneNumber:
+ *         type: 'string'
+ *       courses:
+ *         type: 'array'
+ *         items:
+ *           type: 'string'
+ *
+ */
+
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
